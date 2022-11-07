559bc3bfde29dabd4e1a242a0d000af55eb3432d
Sadia88 <sadiasharmin460280@gmail.com>
2022-11-08 02:42:56 +0600

    navbar is added

---
+++ src/Routes/Routes.js
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import AddServices from "../pages/AddServices/AddServices";
+import Blog from "../pages/Blog/Blog";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Login/Signup";
+import NotFound from "../pages/NotFound/NotFound";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path: "/",
+            element: <Home></Home>, 
+        },
+        {
+            path: "/home",
+            element: <Home></Home>, 
+        },
+        {
+            path: "/login",
+            element:<Login></Login> , 
+        },
+        {
+            path: "/signup",
+            element: <Signup></Signup>, 
+        },
+        {
+            path: "/service",
+            element: <AddServices></AddServices>, 
+        },
+        {
+            path: "/blog",
+            element: <Blog></Blog>, 
+        },
+        {
+            path:'/*',
+            element: <NotFound></NotFound>
+        }
+      ]
+    },
+  ]);
+  export default router;  
